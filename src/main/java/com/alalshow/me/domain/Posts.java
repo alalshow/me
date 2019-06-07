d11edf9f7dfa0e4aabebbeb420bc168519ca3c71
alalshow <alalshow@hanmail.net>
2019-06-07 16:49:40 +0900

    lombok 테스트완료

---
+++ src/main/java/com/alalshow/me/domain/Posts.java
@@ -0,0 +1,39 @@
+package com.alalshow.me.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@Entity
+public class Posts {
+
+@Id
+@GeneratedValue
+private Long id;
+
+@Column(length = 500, nullable = false)
+private String title;
+
+@Column(columnDefinition = "TEXT", nullable = false)
+private String content;
+
+private String author;
+
+@Builder
+public Posts(String title, String content, String author) {
+	this.title = title;
+	this.content = content;
+	this.author = author;
+	}
+
+}
