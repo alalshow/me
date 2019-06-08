04e7e42fd5088389de4f3b49605355b31f1dd32b
alalshow <alalshow@hanmail.net>
2019-06-08 16:28:19 +0900

    소스 정리

--- src/main/java/com/alalshow/me/domain/Posts.java
+++ src/main/java/com/alalshow/me/domain/Posts.java
@@ -16,23 +16,23 @@ import lombok.Setter;
 @Entity
 public class Posts {
 
-@Id
-@GeneratedValue
-private Long id;
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(length = 500, nullable = false)
+	private String title;
+	
+	@Column(columnDefinition = "TEXT", nullable = false)
+	private String content;
+	
+	private String author;
 
-@Column(length = 500, nullable = false)
-private String title;
-
-@Column(columnDefinition = "TEXT", nullable = false)
-private String content;
-
-private String author;
-
-@Builder
-public Posts(String title, String content, String author) {
-	this.title = title;
-	this.content = content;
-	this.author = author;
+	@Builder
+	public Posts(String title, String content, String author) {
+		this.title = title;
+		this.content = content;
+		this.author = author;
 	}
 
 }
