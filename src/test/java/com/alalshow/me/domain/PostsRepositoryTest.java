1ebff824d2c88d6e44d62f3d3361eba28e84f446
alalshow <alalshow@hanmail.net>
2019-06-08 17:12:25 +0900

    test 코드 변경

--- src/test/java/com/alalshow/me/domain/PostsRepositoryTest.java
+++ src/test/java/com/alalshow/me/domain/PostsRepositoryTest.java
@@ -18,46 +18,20 @@ public class PostsRepositoryTest {
 
 	@Autowired
 	PostsRepository postsRepository;
-	
+
 	@After
 	public void cleanup() {
-		/**
-		이후 테스트 코드에 영향을 끼치지 않기 위해
-		테스트 메소드가 끝날때 마다 respository 전체 비우는 코드
-		**/
-		postsRepository.deleteAll();
+		postsRepository.deleteAll(); // 이후 테스트 코드에 영향을 끼치지 않기 위해 테스트 메소드가 끝날때 마다 respository 전체 비우는 코드
 	}
-	
-	@Test
-	public void 게시글_저장() {
-		//given
-		postsRepository.save(Posts.builder()
-		.title("테스트 게시글")
-		.content("테스트 본문")
-		.author("alalshow211@gmail.com")
-		.build());
-		
-	}
-	
-	@Test
-	public List<Posts> 게시글_불러오기() {
-//		게시글저장_불러오기();
-		System.out.println("---게시글_불러오기---");
-		List<Posts> postsList = postsRepository.findAll();
-		System.out.println("---게시글_사이즈:---" + postsList.size());
-		postsList.stream().forEach((c) -> System.out.println(c.toString()));
-		return postsList;
-		
-	}
-	
+
 	@Test
 	public void 게시글저장_불러오기() {
-		System.out.println("게시글저장_불러오기");
 		//given
-		게시글_저장();
+		postsRepository.save(Posts.builder().title("테스트 게시글").content("테스트 본문").author("jojoldu@gmail.com").build());
+
 		//when
-		List<Posts> postsList = 게시글_불러오기();
-		
+		List<Posts> postsList = postsRepository.findAll();
+
 		//then
 		Posts posts = postsList.get(0);
 		assertThat(posts.getTitle(), is("테스트 게시글"));
